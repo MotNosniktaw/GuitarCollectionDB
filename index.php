c59d57525b585afca4fe16fccd78df01f6b25423
Tom Watkinson <tomwatkinson90@gmail.com>
2019-08-05 15:03:09 +0100

    created function that pulls all rewuired info from from database

--- index.php
+++ index.php
@@ -8,3 +8,30 @@ $db = new PDO(
 
 $db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
 
+/**
+ *
+ * Returns all desired item values for all items.
+ * Where many images are stored for an item, only the first added (MAIN image) is returned
+ *
+ * @param $database
+ * @return array
+ */
+function getAllFromDatabase($database):array
+{
+    $sqlGetAll = $database->prepare(
+        'SELECT `guitars`.`id`, `images`.`fileLocation`, `brands`.`brand`, `guitars`.`model`, `guitars`.`year`, `types`.`type`,
+                    `countries`.`country`, `guitars`.`value`, `guitars`.`dateAcquired` FROM `guitars`
+            LEFT JOIN `images`
+                ON `images`.`guitarID` = `guitars`.`id`
+            LEFT JOIN `brands`
+                ON `brands`.`id` = `guitars`.`brandID`
+            LEFT JOIN `types`
+                ON `types`.`id` = `guitars`.`typeID`
+            LEFT JOIN `countries`
+                ON `countries`.`id` = `guitars`.`countryID`
+            GROUP BY `guitars`.`id`;'
+    );
+
+    $sqlGetAll->execute();
+    return $sqlGetAll->fetchAll();
+}
