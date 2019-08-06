099bff01a770533e17d4c69e0ca5bc1123f291bc
Tom Watkinson <tomwatkinson90@gmail.com>
2019-08-06 13:58:37 +0100

    created new function called getDB() that creates and returns new instance of PDO fixed reference to normalize stylesheet added creation of PDO instance $db

--- index.php
+++ index.php
@@ -1,13 +1,16 @@
 <?php
 
-$db = new PDO
-(
-    'mysql:host=192.168.20.20; dbname=Guitars',
-    'root',
-    ''
-);
-
-$db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+function getDB()
+{
+    $database = new PDO
+    (
+        'mysql:host=192.168.20.20; dbname=Guitars',
+        'root',
+        ''
+    );
+    $database->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+    return $database;
+}
 
 /**
  *
@@ -62,21 +65,18 @@ function buildTable($guitarsArray) {
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Guitar Collection</title>
-    <link rel="stylesheet" type="text/css" href="normalize-8-0-1 copy.css">
+    <link rel="stylesheet" type="text/css" href="normalize-8-0-1.css">
     <link rel="stylesheet" type="text/css" href="styles.css">
 </head>
 <body>
 <div>
-    <h1>
-        Guitar Colleciton
-    </h1>
-    <p>
-        There are the things in my collection
-    </p>
+    <h1>Guitar Colleciton</h1>
+    <p>There are the things in my collection</p>
 </div>
 <div>
     <table>
         <?php
+        $db = getDB();
         $allGuitars = getAllFromDatabase($db);
         buildTable($allGuitars);
         //code to create <tr></tr> elements and fill them with <td> and <th> elements containing item information and column headings
