c45b865c82ac26ce1c9bb75292a140efbe84e78f
Tom Watkinson <tomwatkinson90@gmail.com>
2019-08-07 14:36:37 +0100

    deleted outdated database output version removed commented CSS deleted redundant font libraries removed excess css entries in row class

--- index.php
+++ index.php
@@ -10,7 +10,7 @@ require_once 'functions.php';
     <title>Guitar Collection</title>
     <link rel="stylesheet" type="text/css" href="normalize-8-0-1.css">
     <link rel="stylesheet" type="text/css" href="styles.css">
-    <link href="https://fonts.googleapis.com/css?family=Barlow|Cairo|Catamaran|Maven+Pro|Rajdhani&display=swap"
+    <link href="https://fonts.googleapis.com/css?family=Catamaran&display=swap"
           rel="stylesheet">
 </head>
 <body>
