cbf3c8f99d00a0c081adb36ed7336012e78488f9
Tom Watkinson <tomwatkinson90@gmail.com>
2019-08-08 10:54:07 +0100

    fixed formatting issues detailed in SJR review

--- functions.php
+++ functions.php
@@ -4,10 +4,8 @@
  *
  * @return PDO
  */
-function getDB()
-{
-    $database = new PDO
-    (
+function getDB() {
+    $database = new PDO(
         'mysql:host=192.168.20.20; dbname=Guitars',
         'root',
         ''
@@ -24,8 +22,7 @@ function getDB()
  * @param $database - this is a PDO established connection to the 'Guitars' database
  * @return array - this contains all information for all guitars in an associative array
  */
-function getAllFromDatabase(PDO $database):array
-{
+function getAllFromDatabase(PDO $database):array {
     $query = $database->prepare(
         'SELECT `guitars`.`id`, `images`.`fileLocation`, `brands`.`brand`, `guitars`.`model`, `guitars`.`year`, `types`.`type`,
                     `countries`.`country`, `guitars`.`value`, `guitars`.`dateAcquired` FROM `guitars`
@@ -53,30 +50,31 @@ function getAllFromDatabase(PDO $database):array
  */
 function displayGuitars(array $guitarsArray):string {
     $result = '';
-        foreach ($guitarsArray as $guitar) {
-            $x = array_search($guitar, $guitarsArray);
-            if (isset($guitarsArray[$x]['id'])
-                && isset($guitarsArray[$x]['fileLocation'])
-                && isset($guitarsArray[$x]['brand'])
-                && isset($guitarsArray[$x]['model'])
-                && isset($guitarsArray[$x]['year'])
-                && isset($guitarsArray[$x]['type'])
-                && isset($guitarsArray[$x]['country'])
-                && isset($guitarsArray[$x]['value'])
-                && isset($guitarsArray[$x]['dateAcquired'])) {
-                $result .= '<div class="row item">';
-                $result .= '<div class="item-detail number-column">' . $guitar['id'] . '</div>';
-                $result .= '<div class="item-detail img-column"><img src="' . $guitar['fileLocation'] . '"></div>';
-                $result .= '<div class="item-detail name-column">' . $guitar['brand'] . ' ' . $guitar['model'] . '</div>';
-                $result .= '<div class="item-detail year-column">' . $guitar['year'] . '</div>';
-                $result .= '<div class="item-detail type-column">' . $guitar['type'] . '</div>';
-                $result .= '<div class="item-detail country-column">' . $guitar['country'] . '</div>';
-                $result .= '<div class="item-detail value-column">£' . $guitar['value'] . '.00</div>';
-                $result .= '<div class="item-detail date-acq-column">' . $guitar['dateAcquired'] . '</div>';
-                $result .= '</div>';
-            } else {
-                return 'Cannot display required data. Please contact administrator';
-            }
+    foreach ($guitarsArray as $guitar) {
+        $x = array_search($guitar, $guitarsArray);
+        if (isset($guitarsArray[$x]['id'])
+            && isset($guitarsArray[$x]['fileLocation'])
+            && isset($guitarsArray[$x]['brand'])
+            && isset($guitarsArray[$x]['model'])
+            && isset($guitarsArray[$x]['year'])
+            && isset($guitarsArray[$x]['type'])
+            && isset($guitarsArray[$x]['country'])
+            && isset($guitarsArray[$x]['value'])
+            && isset($guitarsArray[$x]['dateAcquired'])
+        ) {
+            $result .= '<div class="row item">';
+            $result .= '<div class="item-detail number-column">' . $guitar['id'] . '</div>';
+            $result .= '<div class="item-detail img-column"><img src="' . $guitar['fileLocation'] . '"></div>';
+            $result .= '<div class="item-detail name-column">' . $guitar['brand'] . ' ' . $guitar['model'] . '</div>';
+            $result .= '<div class="item-detail year-column">' . $guitar['year'] . '</div>';
+            $result .= '<div class="item-detail type-column">' . $guitar['type'] . '</div>';
+            $result .= '<div class="item-detail country-column">' . $guitar['country'] . '</div>';
+            $result .= '<div class="item-detail value-column">£' . $guitar['value'] . '.00</div>';
+            $result .= '<div class="item-detail date-acq-column">' . $guitar['dateAcquired'] . '</div>';
+            $result .= '</div>';
+        } else {
+            return 'Cannot display required data. Please contact administrator';
         }
-        return $result;
+    }
+    return $result;
     }
