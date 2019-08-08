daea0a8454d5e0c0ca012e2833f22f5559c7079f
Tom Watkinson <tomwatkinson90@gmail.com>
2019-08-08 11:12:38 +0100

    added simple input form

--- index.php
+++ index.php
@@ -18,25 +18,37 @@ $allGuitars = getAllFromDatabase($db);
 </head>
 <body>
 <div class="container">
-<div>
-    <h1>Guitar Collection</h1>
-    <p>These are the things in my collection:</p>
-</div>
-<div class="guitars-container">
-    <div class="row header">
-        <div class="item-detail number-column">#</div>
-        <div class="item-detail img-column">Image</div>
-        <div class="item-detail name-column">Guitar</div>
-        <div class="item-detail year-column">Year</div>
-        <div class="item-detail type-column">Type</div>
-        <div class="item-detail country-column">Country</div>
-        <div class="item-detail value-column">Value</div>
-        <div class="item-detail date-acq-column">Date Acquired</div>
+    <div>
+        <h1>Guitar Collection</h1>
+        <p>These are the things in my collection:</p>
     </div>
-    <?php
-    echo displayGuitars($allGuitars);
-    ?>
-</div>
+    <div>
+        <form action="index.php" method="get">
+            <div>Brand:    <input type="text" name="brand" value="<?php echo $_GET['brand']?>"></div>
+            <div>Model:    <input type="text" name="model" value="<?php echo $_GET['model']?>"></div>
+            <div>Type:     <input type="text" name="type" value="<?php echo $_GET['type']?>"></div>
+            <div>Country:  <input type="text" name="country" value="<?php echo $_GET['country']?>"></div>
+            <div>Year:     <input type="text" name="year" value="<?php echo $_GET['year']?>"></div>
+            <div>Value:    <input type="text" name="value" value="<?php echo $_GET['value']?>"></div>
+            <div>Image:    <input type="text" name="img" value="<?php echo $_GET['img']?>"></div>
+            <div><input type="submit" name="newGuitar"></div>
+        </form>
+    </div>
+    <div class="guitars-container">
+        <div class="row header">
+            <div class="item-detail number-column">#</div>
+            <div class="item-detail img-column">Image</div>
+            <div class="item-detail name-column">Guitar</div>
+            <div class="item-detail year-column">Year</div>
+            <div class="item-detail type-column">Type</div>
+            <div class="item-detail country-column">Country</div>
+            <div class="item-detail value-column">Value</div>
+            <div class="item-detail date-acq-column">Date Acquired</div>
+        </div>
+        <?php
+        echo displayGuitars($allGuitars);
+        ?>
+    </div>
 </div>
 
 </body>
