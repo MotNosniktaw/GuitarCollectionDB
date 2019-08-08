179795e82e0a40e03aa232006abd59b7a8e1fc53
Tom Watkinson <tomwatkinson90@gmail.com>
2019-08-08 15:48:11 +0100

    g

--- addNewGuitar.php
+++ addNewGuitar.php
@@ -9,53 +9,68 @@ $brandInDB = checkBrands($db);
 $typeInDB = checkTypes($db);
 $countryInDB = checkCountries($db);
 
-if ($brandInDB === true && $typeInDB === true && $countryInDB === true) {
-     $message .= '<p>I can do this because you have provided values we can use</p>';
-} else {
-    $message .= '<p>You should add better information</p>';
-}
-echo $message;
-
-//if (isset($_GET['newGuitar'])
-//    && isset($_GET['brand'])
-//    && isset($_GET['model'])
-//    && isset($_GET['year'])
-//    && isset($_GET['type'])
-//    && isset($_GET['country'])
-//    && isset($_GET['hand'])
-//    && isset($_GET['value'])
-//    && isset($_GET['date'])) {
-//    ;
-//}
-
 $brand = getBrandID($db);
-echo $brand . '<br><br>';
 $type = getTypeID($db);
-echo $type . '<br><br>';
 $country = getCountryID($db);
-echo $country . '<br><br>';
-addNewGuitar($db, $brand, $type, $country);
-addNewImage($db);
 
+if (isset($_GET['newGuitar'])
+    && isset($_GET['brand'])
+    && isset($_GET['model'])
+    && isset($_GET['year'])
+    && isset($_GET['type'])
+    && isset($_GET['country'])
+    && isset($_GET['hand'])
+    && isset($_GET['value'])
+    && isset($_GET['date'])
+) {
+    if ($brandInDB === true && $typeInDB === true && $countryInDB === true) {
+        $message .= '<p>I can do this because you have provided values we can use</p>';
+        $brand = getBrandID($db);
+        $type = getTypeID($db);
+        $country = getCountryID($db);
+
+        addNewGuitar($db, $brand, $type, $country);
+        addNewImage($db);
+    } else {
+        $message .= '<p>You should add better information</p>';
+    }
+}
 ?>
-<div>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Guitar Collection - Add</title>
+    <link href="https://fonts.googleapis.com/css?family=Catamaran&display=swap"
+          rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="stylesheets/normalize-8-0-1.css">
+    <link rel="stylesheet" type="text/css" href="stylesheets/styles.css">
+</head>
+<body>
+<div class="container">
+<?php
+    echo $message;
+    ?>
     <form action="addNewGuitar.php" method="get">
-        <div>Brand: <input type="text" name="brand" value="<?php echo $_GET['brand']?>" required></div>
-        <div>Model: <input type="text" name="model" value="<?php echo $_GET['model']?>" required></div>
-        <div>Year: <input type="text" name="year" value="<?php echo $_GET['year']?>" required></div>
-        <div>Type: <input type="text" name="type" value="<?php echo $_GET['type']?>" required></div>
-        <div>Country: <input type="text" name="country" value="<?php echo $_GET['country']?>" required></div>
+        <div>Brand: <input type="text" name="brand" required></div>
+        <div>Model: <input type="text" name="model" required></div>
+        <div>Year: <input type="number" name="year" required></div>
+        <div>Type: <input type="text" name="type" required></div>
+        <div>Country: <input type="text" name="country" required></div>
         <div>LH or RH:
-            <select name="hand" value="<?php echo $_GET['hand']?>" required>
-                <option value="0">Please select LH or RH</option>
+            <select name="hand" required>
                 <option value="LH">LH</option>
                 <option value="RH">RH</option>
             </select></div>
-        <div>Value: <input type="number" name="value" value="<?php echo $_GET['value']?>" required></div>
-        <div>Serial Code: <input type="text" name="serial" value="<?php echo $_GET['serial']?>"></div>
-        <div>Image: <input type="text" name="img" value="<?php echo $_GET['img']?>"></div>
-        <div>Date Acquired: <input type="date" name="date" value="<?php echo $_GET['date']?>"></div>
+        <div>Value: <input type="number" name="value" required></div>
+        <div>Serial Code: <input type="text" name="serial"></div>
+        <div>Image: <input type="text" name="img"?></div>
+        <div>Date Acquired: <input type="date" name="date" required></div>
         <div><input type="submit" name="newGuitar"></div>
     </form>
+
+    <a href="index.php">Go back to DB to see items that have acquired</a>
 </div>
-<a href="index.php">Go back to DB</a>
+</body>
+</html>
+
--- index.php
+++ index.php
@@ -22,26 +22,7 @@ $allGuitars = getAllFromDatabase($db);
         <h1>Guitar Collection</h1>
         <p>These are the things in my collection:</p>
     </div>
-    <div>
-        <form action="addNewGuitar.php" method="get">
-            <div>Brand: <input type="text" name="brand" value="<?php echo $_GET['brand']?>" required></div>
-            <div>Model: <input type="text" name="model" value="<?php echo $_GET['model']?>" required></div>
-            <div>Year: <input type="text" name="year" value="<?php echo $_GET['year']?>" required></div>
-            <div>Type: <input type="text" name="type" value="<?php echo $_GET['type']?>" required></div>
-            <div>Country: <input type="text" name="country" value="<?php echo $_GET['country']?>" required></div>
-            <div>LH or RH:
-                <select name="hand" value="<?php echo $_GET['hand']?>" required>
-                    <option value="0">Please select LH or RH</option>
-                    <option value="LH">LH</option>
-                    <option value="RH">RH</option>
-                </select></div>
-            <div>Value: <input type="number" name="value" value="<?php echo $_GET['value']?>" required></div>
-            <div>Serial Code: <input type="text" name="serial" value="<?php echo $_GET['serial']?>"></div>
-            <div>Image: <input type="text" name="img" value="<?php echo $_GET['img']?>"></div>
-            <div>Date Acquired: <input type="date" name="date" value="<?php echo $_GET['date']?>"></div>
-            <div><input type="submit" name="newGuitar"></div>
-        </form>
-    </div>
+    <div><a href="addNewGuitar.php">Add a new item</a></div>
     <div class="guitars-container">
         <div class="row header">
             <div class="item-detail number-column">#</div>
